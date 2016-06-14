9cc5183f664630a8e39032bcc9ed52e3f754bc48
Gautam Machingal <gautam.krishna@gmail.com>
2016-06-14 16:47:22 -0700

    - MaxSizeExceeded Exception handling - Pop up for disclaimer

--- main/src/main/java/com/luvbrite/config/GlobalExceptionHandler.java
+++ main/src/main/java/com/luvbrite/config/GlobalExceptionHandler.java
@@ -4,10 +4,15 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-	
+    
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public String maxSizeExceeded(HttpServletRequest request, Exception ex){        
+        return "misc/upload-size-error";
+    }
     
     @ExceptionHandler
     public String handleException(HttpServletRequest request, Exception ex){
