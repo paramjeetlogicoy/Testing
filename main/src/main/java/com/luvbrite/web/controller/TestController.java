1f21f319f34c266fb48e0fbd852377f7cc1f0e9c
Prashant <prashant.kumar@logicoy.com>
2019-10-11 00:39:48 +0530

    Tookan Webhook Reciever

--- main/src/main/java/com/luvbrite/web/controller/TestController.java
+++ main/src/main/java/com/luvbrite/web/controller/TestController.java
@@ -10,43 +10,53 @@ import com.luvbrite.dao.OrderDAO;
 import com.luvbrite.services.PostOrderMeta;
 import com.luvbrite.web.models.GenericResponse;
 import com.luvbrite.web.models.Order;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping(value = "/testTookanWebhook")
 public class TestController {
-	
-	@Autowired
-	private OrderDAO orderDao;
-	
-	
-	@Autowired
-	private PostOrderMeta postService;	
-	
 
-	@RequestMapping(value = "/test/meta/{orderNumber}")
-	public @ResponseBody GenericResponse emailTemplateTest(@PathVariable Long orderNumber){	
+//    @Autowired
+//    private OrderDAO orderDao;
+//
+//    @Autowired
+//    private PostOrderMeta postService;
 
-		GenericResponse gr  = new GenericResponse();
-		gr.setSuccess(false);
-		gr.setMessage("");
-		
-		if(orderNumber != null){
-			Order order = orderDao.findOne("orderNumber", orderNumber);
-			if(order != null){
-				
-				String resp = postService.postOrder(order);
-				if(resp.equals("success")){
-					gr.setSuccess(true);
-				}
-				else{
-					gr.setMessage(resp);
-				}					
-			}
-		}
-		else{
-			gr.setMessage("Ordernumber is null");
-		}
+//	@RequestMapping(value = "/test/meta/{orderNumber}")
+//	public @ResponseBody GenericResponse emailTemplateTest(@PathVariable Long orderNumber){	
+//
+//		GenericResponse gr  = new GenericResponse();
+//		gr.setSuccess(false);
+//		gr.setMessage("");
+//		
+//		if(orderNumber != null){
+//			Order order = orderDao.findOne("orderNumber", orderNumber);
+//			if(order != null){
+//				
+//				String resp = postService.postOrder(order);
+//				if(resp.equals("success")){
+//					gr.setSuccess(true);
+//				}
+//				else{
+//					gr.setMessage(resp);
+//				}					
+//			}
+//		}
+//		else{
+//			gr.setMessage("Ordernumber is null");
+//		}
+//
+//		return gr;		
+//	}
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public ResponseEntity<String> getTookanNotification(@RequestBody String tookanNotification) {
 
-		return gr;		
-	}
+        System.out.println("tookanNotification:--->" + tookanNotification);
+
+        return new ResponseEntity<String>("Recieved Tookan Response", HttpStatus.OK);
+    }
+
 }
--- main/src/main/java/com/luvbrite/web/models/ordermeta/Order.java
+++ main/src/main/java/com/luvbrite/web/models/ordermeta/Order.java
@@ -5,97 +5,133 @@ import java.util.List;
 import com.luvbrite.web.models.OrderTax;
 
 public class Order {
-	private BillingAddress billing_address = null;
-	private List<LineItem> line_items = null;
-	
-	private String completed_at = "";
-	private String total = "";
-	private String total_discount = "";
-	private String note = "";
-	private String delivery_notes = "";
-	private String status = "";
-	private String paymentMethod = "";
-	private int order_number = 0;
-	private double tax = 0d;
-	private OrderTax orderTax;
-	private double rushFee = 0d;
-	
-	public BillingAddress getBilling_address() {
-		return billing_address;
-	}
-	public void setBilling_address(BillingAddress billing_address) {
-		this.billing_address = billing_address;
-	}
-	public List<LineItem> getLine_items() {
-		return line_items;
-	}
-	public void setLine_items(List<LineItem> line_items) {
-		this.line_items = line_items;
-	}
-	public String getCompleted_at() {
-		return completed_at;
-	}
-	public void setCompleted_at(String completed_at) {
-		this.completed_at = completed_at;
-	}
-	public String getTotal() {
-		return total;
-	}
-	public void setTotal(String total) {
-		this.total = total;
-	}
-	public String getTotal_discount() {
-		return total_discount;
-	}
-	public void setTotal_discount(String total_discount) {
-		this.total_discount = total_discount;
-	}
-	public String getNote() {
-		return note;
-	}
-	public void setNote(String note) {
-		this.note = note;
-	}
-	public String getDelivery_notes() {
-		return delivery_notes;
-	}
-	public void setDelivery_notes(String delivery_notes) {
-		this.delivery_notes = delivery_notes;
-	}
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
-	public int getOrder_number() {
-		return order_number;
-	}
-	public void setOrder_number(int order_number) {
-		this.order_number = order_number;
-	}
-	public String getPaymentMethod() {
-		return paymentMethod;
-	}
-	public void setPaymentMethod(String paymentMethod) {
-		this.paymentMethod = paymentMethod;
-	}
-	public double getTax() {
-		return tax;
-	}
-	public void setTax(double tax) {
-		this.tax = tax;
-	}
-	public OrderTax getOrderTax() {
-		return orderTax;
-	}
-	public void setOrderTax(OrderTax orderTax) {
-		this.orderTax = orderTax;
-	}
-	public double getRushFee() {
-		return rushFee;
-	}
-	public void setRushFee(double rushFee) {
-		this.rushFee = rushFee;
-	}	
+
+    private BillingAddress billing_address = null;
+    private List<LineItem> line_items = null;
+
+    private String email = "";
+    private String completed_at = "";
+    private String total = "";
+    private String total_discount = "";
+    private String note = "";
+    private String delivery_notes = "";
+    private String status = "";
+    private String paymentMethod = "";
+    private int order_number = 0;
+    private double tax = 0d;
+    private OrderTax orderTax;
+    private double rushFee = 0d;
+
+    public BillingAddress getBilling_address() {
+        return billing_address;
+    }
+
+    public void setBilling_address(BillingAddress billing_address) {
+        this.billing_address = billing_address;
+    }
+
+    public List<LineItem> getLine_items() {
+        return line_items;
+    }
+
+    public void setLine_items(List<LineItem> line_items) {
+        this.line_items = line_items;
+    }
+
+    public String getCompleted_at() {
+        return completed_at;
+    }
+
+    public void setCompleted_at(String completed_at) {
+        this.completed_at = completed_at;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public String getTotal_discount() {
+        return total_discount;
+    }
+
+    public void setTotal_discount(String total_discount) {
+        this.total_discount = total_discount;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getDelivery_notes() {
+        return delivery_notes;
+    }
+
+    public void setDelivery_notes(String delivery_notes) {
+        this.delivery_notes = delivery_notes;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getOrder_number() {
+        return order_number;
+    }
+
+    public void setOrder_number(int order_number) {
+        this.order_number = order_number;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public double getTax() {
+        return tax;
+    }
+
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
+
+    public OrderTax getOrderTax() {
+        return orderTax;
+    }
+
+    public void setOrderTax(OrderTax orderTax) {
+        this.orderTax = orderTax;
+    }
+
+    public double getRushFee() {
+        return rushFee;
+    }
+
+    public void setRushFee(double rushFee) {
+        this.rushFee = rushFee;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
